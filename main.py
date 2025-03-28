ad6d0656b5ac93c2c052c0ad01d347d589306cdd
Leonit <74876947+DrkTheDon@users.noreply.github.com>
2025-03-28 10:42:55 +0100

    First commit
    Added basic barebone of the game.

---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+#################################
+# Game: Snake Game              #
+# Purpose: School assignment.   #
+# Author: Leonit Ajvazi         #
+#################################
+
+# Imports
+from tkinter import * # Importing a lib for    
+import random # Importing the random library
+
+# Constants/global vars
+GAME_WIDTH = 700
+GAME_LENGTH = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 1
+FOOD_COLOR = "#FF0000"
+
+# Customizable variables
+snake_color = "#00FF00" # Not a const, this will be customizable!
+background_color = "#000000"
+
+
+# Classes
+class snake:
+    pass
+class apple:
+    pass
+
+
+
+
+# Functions
+def options():
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direciton):
+    pass
+
+def chack_collision():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Snake Game - Leonit Ajvazi")
+window.resizable(False, False)
+
+window.mainloop()
