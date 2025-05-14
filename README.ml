94806b520ab27eebe6d80772734d29ceeaa9effd
Leonit <74876947+DrkTheDon@users.noreply.github.com>
2025-05-14 08:08:13 +0200

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,10 +1,10 @@
 # USAGE
 
-##CD INTO DIRECTORY
+# CD INTO DIRECTORY
 cd path/to/snakegame
 
-##INSTALL LIBRARIES
+# INSTALL LIBRARIES
 pip install -r requirements.txt
 
-##RUN THE GAME
-```python3 main.py```
+# RUN THE GAME
+python3 main.py
